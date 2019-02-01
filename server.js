26d2e67d4491efd42c8ca51d5ac785a3c73ea553
Desi Mandasari <desimandasari1993@gmail.com>
2019-02-01 14:19:47 +0700

    add cors

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
