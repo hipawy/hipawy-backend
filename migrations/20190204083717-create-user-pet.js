100c86ab9b6121e0992dbb9442f8dba4c55b2dae
Dheta Catur Maharani <theresiadhetacm@gmail.com>
2019-02-04 15:49:25 +0700

    add key reference in userpet migration

--- migrations/20190204083717-create-user-pet.js
+++ migrations/20190204083717-create-user-pet.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('userPets', {
+    return queryInterface.createTable("userPets", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -18,10 +18,28 @@ module.exports = {
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
+      },
+      id_user: {
+        type: Sequelize.INTEGER,
+        onDelete: "CASCADE",
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id"
+        }
+      },
+      id_pet: {
+        type: Sequelize.INTEGER,
+        onDelete: "CASCADE",
+        allowNull: false,
+        references: {
+          model: "pets",
+          key: "id"
+        }
       }
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('userPets');
+    return queryInterface.dropTable("userPets");
   }
 };
