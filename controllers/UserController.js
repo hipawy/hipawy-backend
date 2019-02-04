484462f288846ee3f5e039c3eead781b8134240b
Dheta Catur Maharani <theresiadhetacm@gmail.com>
2019-02-04 16:02:38 +0700

    create controller to read user by id

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,23 @@
-const { user } = require("../models");
+const { sequelize } = require("../models");
+const User = require("../models").user;
+const Pet = require("../models").pet;
+const UserPet = require("../models").userPet;
 
 exports.getUsers = async (req, res) => {
   try {
     const users = await user.findAll();
 
     res.status(200).json({ users });
+  } catch (err) {
+    res.status(500).json({ message: "There is an error.", err });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    res.status(200).json({ user });
   } catch (err) {
     res.status(500).json({ message: "There is an error.", err });
   }
