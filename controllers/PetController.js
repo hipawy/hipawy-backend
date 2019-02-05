a237aadd3cc1d704648b22c3f5b655a0aa656998
Dheta Catur Maharani <theresiadhetacm@gmail.com>
2019-02-05 21:16:57 +0700

    modify pet controller

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -1,12 +1,21 @@
-const { sequelize } = require("../models");
 const Pet = require("../models").pet;
 
 exports.getPets = async (req, res) => {
-  try {
-    const pets = await Pet.findAll();
+  if (req.query) {
+    let condition = {};
 
-    res.status(200).json({ pets });
-  } catch (err) {
-    res.status(500).json({ message: "There is an error.", err });
+    Object.keys(req.query).forEach(key =>
+      Object.assign(condition, {
+        [key]: { $like: `%${req.query[key]}%` }
+      })
+    );
+
+    Pet.findAll({ where: { ...condition } })
+      .then(pets => res.status(200).json({ pets }))
+      .catch(err => res.status(500).json(err));
+  } else {
+    Pet.findAll()
+      .then(pets => res.status(200).json({ pets }))
+      .catch(err => res.status(500).json(err));
   }
 };
